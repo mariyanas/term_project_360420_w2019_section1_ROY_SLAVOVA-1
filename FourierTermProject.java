cbf1bc616c067f872a2e51cb3cb2556c9b63c14f
mariyanas <psmariyana@gmail.com>
2019-05-15 20:47:58 -0400

    made changes to the code, added output file to get the power and frequency, change the frequency calcualtion because there was a mistake

--- FourierTermProject.java
+++ FourierTermProject.java
@@ -24,6 +24,21 @@ public class FourierTermProject {
 
 	// Start main method
 	public static void main(String[] Args) {
+		
+		//*********************************************************************
+        //Opening a file to store data ****************************************
+        //*********************************************************************
+        String filename = "powerAndFrequencyOutput.txt";
+        PrintWriter outputFile = null;
+        try
+        {
+            outputFile = new PrintWriter(new FileOutputStream(filename,false));
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("File error.  Program aborted.");
+            System.exit(0);
+        }
 
 		
 		System.out.print("Path and Name of Selected File is '");
@@ -85,11 +100,20 @@ public class FourierTermProject {
 
 			// Close the WAV file
 			wavFile.close();
+			
+							//create a buffered reader that connects to the console, we use it so we can read lines
+				BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+				//read a line from the console
+				String lineFromInput = in.readLine();
+
+			
+
 		}
 		catch (Exception e) {
 			System.err.println(e);
 		}
-		
+
 		
 		//////////////////////////////////////		
 		// 2. Padding
@@ -150,16 +174,96 @@ public class FourierTermProject {
 
 		// Calculate frequency and power and fill the arrays
 		for (int m = 0; m < frequency.length; m++) {
-			frequency[m] = m/dt/signalFT.length;
+			frequency[m] = /*m/dt/signalFT.length*/ m*fNyq/frequency.length;
 			power[m] = Math.pow(signalFT[m].getReal(),2)+Math.pow(signalFT[m].getImaginary(),2);
 		}
 		
+		//************************************************************************************************
+		//open this part to get the power data in the file and close it to when getting the frequency data
+		double p = 0;
 		for (int i = 0; i < frequency.length; i++){
-		System.out.println (power[i]);
+		//System.out.println (power[i]);
+		p = power[i] ;
 		
-
-   
+		outputFile.printf("%6.6f\n",p);
 		}
+		
+		//************************************************************************************************
+		//open this part to get the frequency data in the file and close it to when getting the power data
+		/*double f = 0;
+		for (int i = 0; i < frequency.length; i++){
+		f = frequency[i] ;
+		
+		outputFile.printf("%6.6f\n",f);
+		}*/
+		
+		outputFile.close();
+		
+		
+		
+		//////////////////////////////////////		
+		// 5. Plotting (DO NOT EDIT)
+		//////////////////////////////////////
+
+		//Font plotFont = new Font(Font.MONOSPACED,Font.PLAIN,12);
+
+		// Signal plot
+		/*
+		// Create a PlotPanel
+  		Plot2DPanel plot1 = new Plot2DPanel();
+		
+		// Add a line plot and labels to the PlotPanel
+		plot1.addLinePlot("Signal",Color.magenta,time,signal);
+		plot1.setFixedBounds(0,0,signal.length*dt); //Manually set bounds on x-axis	
+		plot1.setAxisLabels("Time", "Amplitude");
+		plot1.getAxis(0).setLabelPosition(0.5, -0.1);
+		plot1.getAxis(0).setLabelFont(plotFont);
+		plot1.getAxis(1).setLabelPosition(-0.15,0.5);
+		plot1.getAxis(1).setLabelFont(plotFont);
+		BaseLabel title1 = new BaseLabel("Signal from " + FileChosen,Color.BLACK,0.5, 1.1);
+		title1.setFont(plotFont);
+       		plot1.addPlotable(title1);
+			
+		// Put the PlotPanel in a JFrame as a JPanel
+		JFrame frame1 = new JFrame("Output 1");
+		frame1.setSize(1024,576);
+		frame1.setContentPane(plot1);
+		frame1.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame1.setVisible(true);
+
+		// Power spectrum plot
+
+		// Find maximum power
+		double PMax = 0.0;
+		for (int j = 0; j < power.length; j++) {
+			if (power[j] > PMax) {
+				PMax = power[j];
+			}
+		} 
+		
+		// Create a PlotPanel (you can use it as a JPanel)
+  		Plot2DPanel plot2 = new Plot2DPanel();
+				
+		// Add a bar plot and labels to the PlotPanel
+		plot2.addLinePlot("Power", frequency, power);
+		plot2.setFixedBounds(0,0,fNyq);	//Manually set bounds on x-axis
+		plot2.setFixedBounds(1,0,1.01*PMax); // Manually set bounds on y-axis
+		plot2.setAxisLabels("Frequency", "Power");
+		plot2.getAxis(0).setLabelPosition(0.5,-0.1);
+		plot2.getAxis(0).setLabelFont(plotFont);
+		plot2.getAxis(1).setLabelPosition(-0.15,0.5);
+		plot2.getAxis(1).setLabelFont(plotFont);
+		BaseLabel title2 = new BaseLabel("Power spectrum of " + FileChosen, Color.BLACK, 0.5, 1.1);
+		title2.setFont(plotFont);
+       		plot2.addPlotable(title2);
+		
+		// Put the PlotPanel in a JFrame, as a JPanel
+		JFrame frame2 = new JFrame("Output 2");
+		frame2.setSize(1024,576);
+		frame2.setLocationRelativeTo(null);
+		frame2.setContentPane(plot2);
+		frame2.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		frame2.setVisible(true);*/
 	}
 	
 	private static File getFile()
@@ -167,7 +271,7 @@ public class FourierTermProject {
     // Since our code will not be tested on our own computer, it is important that we allow the teacher to chose it himself from our folder
     // (Trying to get the file from a specific directory would result problematic)
     {
-        JFileChooser fc = new JFileChooser(); // Empty constructor that points to userâ€™s default directory
+        JFileChooser fc = new JFileChooser(); // Empty constructor that points to user’s default directory
         int result = fc.showOpenDialog(null); // Pops up an "Open File" file chooser dialog; "null" means displayed at the center
         File file = null; // Declare "file"; "null" means empty object file
         if (result == JFileChooser.APPROVE_OPTION) // APPROVE_OPTION: Return value if approve (yes, ok) is chosen.
